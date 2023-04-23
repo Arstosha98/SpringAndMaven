e4c637c73a985d371f0018c6ee41d7260085584b
Ars_tosha <arsentev98pochta@mail.ru>
2023-04-23 16:03:33 +0500

    Start

---
+++ src/main/java/pro/sky/skyprospringandmaven/SkyproSpringAndMavenApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringandmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringAndMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringAndMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringandmaven/SkyproSpringAndMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringandmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringAndMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
